f6df551b3b4195bb854fca85bd5e09234ade3534
sheffer21 <alonsh9@gmail.com>
2020-03-20 15:54:47 +0200

    update fixes

--- MainProgram/Main.py
+++ MainProgram/Main.py
@@ -11,34 +11,34 @@ def main():
     logger.printAndLog(const.MessageType.Summarize, "Welcome to our project: \"Stock prediction by twitter\" :)")
     logger.printAndLog(const.MessageType.printLog, "")
 
-    tweetsCrawler = input(const.MAIN_HEADER + "Do you want to run the twitter crawler? \n"
-                                              "Twitter crawler uses twitter's REST API "
-                                              "to fetch new tweets.\n"
-                                              "Type <yes/no>: " + const.MAIN_ENDC)
+    # tweetsCrawler = input(const.MAIN_HEADER + "Do you want to run the twitter crawler? \n"
+    #                                          "Twitter crawler uses twitter's REST API "
+    #                                          "to fetch new tweets.\n"
+    #                                          "Type <yes/no>: " + const.MAIN_ENDC)
 
-    if tweetsCrawler == "yes":
-        tc.main(logger)
-    else:
-        logger.printAndLog(const.MessageType.Summarize, "TweetsCrawler not ran")
+    # if tweetsCrawler == "yes":
+    #     tc.main(logger)
+    # else:
+    #     logger.printAndLog(const.MessageType.Summarize, "TweetsCrawler not ran")
 
-    runPreProcessing = input(const.MAIN_HEADER + "Do you want to run the Preprocessor? \n"
-                                                 "Preprocessor uses fetched databases to build and analyze "
-                                                 "stocks database prior the learning stage. \n"
-                                                 "Type <yes/no>: " + const.MAIN_ENDC)
+    # runPreProcessing = input(const.MAIN_HEADER + "Do you want to run the Preprocessor? \n"
+    #                                              "Preprocessor uses fetched databases to build and analyze "
+    #                                              "stocks database prior the learning stage. \n"
+    #                                              "Type <yes/no>: " + const.MAIN_ENDC)
 
-    if runPreProcessing == "yes":
-        init.main(logger)
-    else:
-        logger.printAndLog(const.MessageType.Summarize, "Preprocessor not ran")
+    # if runPreProcessing == "yes":
+    #     init.main(logger)
+    # else:
+    #     logger.printAndLog(const.MessageType.Summarize, "Preprocessor not ran")
 
-    runMachineLearning = input(const.MAIN_HEADER + "Do you want to run the machine learner? \n"
-                                                   "description \n"
-                                                   "Type <yes/no>: " + const.MAIN_ENDC)
+    # runMachineLearning = input(const.MAIN_HEADER + "Do you want to run the machine learner? \n"
+    #                                                "description \n"
+    #                                                "Type <yes/no>: " + const.MAIN_ENDC)
 
-    if runMachineLearning == "yes":
-        learner.main(logger)
-    else:
-        logger.printAndLog(const.MessageType.Summarize, "MachineLearner not ran")
+    # if runMachineLearning == "yes":
+    learner.main(logger)
+    # else:
+    #    logger.printAndLog(const.MessageType.Summarize, "MachineLearner not ran")
 
     # runAfterMath = input("Do you want to run after math? \n"
     #                      "After math lets you run operations like looking in graphs and getting statistics"
